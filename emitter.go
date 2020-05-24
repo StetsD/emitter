c4ddb5060dc675deaa30c6ef11c79a8e9dd90688
stdm <must.be.std@gmail.com>
2020-05-24 19:03:02 +0300

    interface changed

--- emitter.go
+++ emitter.go
@@ -1,33 +1,49 @@
 package emitter
 
 type Emitter struct {
-	eMap map[string][]func()
+	eMap map[string][]Listener
 }
 
-func (e *Emitter) On(event string, cb func()) {
+type Listener struct {
+	key string
+	f   *func()
+}
+
+// Subscribe on event
+func (e *Emitter) On(event string, key string, cb func()) {
 	if _, ok := e.eMap[event]; !ok {
-		e.eMap[event] = make([]func(), 0)
+		e.eMap[event] = make([]Listener, 0)
 	}
 
-	e.eMap[event] = append(e.eMap[event], cb)
+	e.eMap[event] = append(e.eMap[event], Listener{key: key, f: &cb})
 }
 
+// Emit event by name and call all listeners
 func (e *Emitter) Emit(event string) {
-	var listeners *[]func()
-
-	for key, cbs := range e.eMap {
-		if key == event {
-			listeners = &cbs
+	if listeners, ok := e.eMap[event]; ok {
+		if listeners != nil {
+			for _, listener := range listeners {
+				(*listener.f)()
+			}
 		}
 	}
+}
 
-	if listeners != nil {
-		for _, cb := range *listeners {
-			cb()
-		}
-	}
+//func (e *Emitter) RemoveListener(event string, rmCb func()) {
+//	if listeners, ok := e.eMap[event]; ok {
+//		for i, cb := range listeners {
+//			fmt.Println(reflect.ValueOf(*cb) == reflect.ValueOf(rmCb), reflect.ValueOf(*cb).Interface() == reflect.ValueOf(rmCb).Interface())
+//			if reflect.ValueOf(*cb).Pointer() == reflect.ValueOf(rmCb).Pointer() {
+//				e.eMap[event] = append(e.eMap[event][:i], e.eMap[event][i+1:]...)
+//			}
+//		}
+//	}
+//}
+
+func (e *Emitter) RemoveAllListeners(event string) {
+	delete(e.eMap, event)
 }
 
 func Create() *Emitter {
-	return &Emitter{make(map[string][]func())}
+	return &Emitter{make(map[string][]Listener)}
 }
--- test/main.go
+++ test/main.go
@@ -9,8 +9,12 @@ type Worker struct {
 	name string
 }
 
-func (w *Worker) work() {
+func (w *Worker) Work() {
 	fmt.Println("I am " + w.name + " and I work msg: ")
+}
+
+func (w *Worker) Notify() {
+	fmt.Println(w.name, " says TurboLove")
 }
 
 type Node struct {
@@ -22,14 +26,22 @@ func (n *Node) message(msg string) {
 	fmt.Println("I am "+n.name+" and I receive msg: ", msg)
 }
 
+func f() {}
+
 func main() {
 	event1 := "superKillMazafakaSignal"
 	worker1 := Worker{"worker_1"}
 	worker2 := Worker{"worker_2"}
 	node1 := Node{"node_1", *emitter.Create()}
 
-	node1.On(event1, worker1.work)
-	node1.On(event1, worker2.work)
+	node1.On(event1, "worker1.Work", worker1.Work)
+	node1.On(event1, "worker2.Work", worker2.Work)
+	node1.On(event1, "worker1.Notify", worker1.Notify)
+	//node1.RemoveListener(event1, worker2.Work)
+	//node1.RemoveAllListeners(event1)
+	//
 
 	node1.Emit(event1)
+	//node1.RemoveListener(event1, worker1.Notify)
+	//node1.Emit(event1)
 }
